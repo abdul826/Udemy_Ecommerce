a7f172b37d606abc27b5b703c6a0f2d833bc1f78
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2023-05-25 17:07:41 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const getProducts = require("../controllers/productController")
 
-router.get("/", (req,res) => {
-    res.send("Handling product routes, e.g. search for products")
-})
+router.get("/category/:categoryName/search/:searchQuery", getProducts)
+router.get("/category/:categoryName", getProducts)
+router.get("/search/:searchQuery", getProducts)
+router.get("/", getProducts)
 
 module.exports = router
