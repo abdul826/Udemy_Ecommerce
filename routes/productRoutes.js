95a05db12c7d01842c4a54e98c59abd3914a5951
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2023-05-26 17:53:18 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const {getProducts,getProductById,getBestSeller} = require("../controllers/productController")
+const {getProducts,getProductById,getBestSeller,adminGetProduct,adminDeleteProduct,adminDeleteProductImage,adminUpdateProduct,adminUpload,adminCreateProduct} = require("../controllers/productController")
 
 router.get("/category/:categoryName/search/:searchQuery", getProducts)
 router.get("/category/:categoryName", getProducts)
 router.get("/search/:searchQuery", getProducts)
 router.get("/", getProducts);
 
-router.get("/:id", getProductById);
-
+router.get("/get-one/:id", getProductById);
 router.get("/bestseller", getBestSeller);
+
+// Admin route
+router.get("/admin", adminGetProduct);
+router.delete("/admin/:id", adminDeleteProduct);
+router.delete("/admin/image/:imagePath/:productId", adminDeleteProductImage);
+router.put("/admin/:id", adminUpdateProduct);
+router.post("/admin/upload", adminUpload);
+router.post("/admin", adminCreateProduct);
 
 module.exports = router
