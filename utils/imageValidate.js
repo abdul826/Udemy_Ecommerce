0ad8c7aef5384d08dc7ede00e0deda00f212c3f9
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2023-05-27 12:11:49 +0530

    Update imageValidate.js

--- utils/imageValidate.js
+++ utils/imageValidate.js
@@ -1,30 +1,23 @@
-const imageValidate = (images)=>{
-    let imageTable = [];
-
-    // If images is more than one else single image 
-    if(Array.isArray(images)){
-        imageTable = images;
-    }else{
-        imageTable.push(images);
+const imageValidate = (images) => {
+    let imagesTable = []
+    if(Array.isArray(images)) {
+        imagesTable = images
+    } else {
+        imagesTable.push(images)
     }
 
-    if(imageTable.length > 3){
-        return {error: "Send only 3 images at once"}
+    if(imagesTable.length > 3) {
+        return { error: "Send only 3 images at once" }
+    }
+    for(let image of imagesTable) {
+        if(image.size > 1048576) return { error: "Size too large (above 1 MB)" }
+
+        const filetypes = /jpg|jpeg|png/
+        const mimetype = filetypes.test(image.mimetype)
+        if(!mimetype) return { error: "Incorrect mime type (should be jpg,jpeg or png" }
     }
 
-    for(let image of imageTable){
-        if(image.size > 1048576){
-            return {error: "Images Size should be less or equal to 1 MB."}
-        }
-
-        const filetypes = /jpg|jpeg|png/;
-        const mimetypes = filetypes.test(image.mimetypes);
-
-        if(!mimetypes) return {error: "Incorrect mime type (Should be jpg,jpeg,png)."}
-    }
-
-    // If no errors Come 
-    return {error: false};
+    return { error: false }
 }
 
-module.exports = imageValidate;
+module.exports = imageValidate
