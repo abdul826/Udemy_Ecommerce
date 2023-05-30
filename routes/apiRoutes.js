c235c49a21e5b323be982228fecd4a92027cb0c6
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2023-05-30 18:19:18 +0530

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const productRoutes = require("./productRoutes")
 
-app.use("/products", productRoutes)
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app
