238380f5222923953b1547cd2390ed8f71cc0460
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2023-05-30 17:32:47 +0530

    Create orderRoutes.js

---
+++ routes/orderRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router();
+const {verifyIsLoggedIn,verifyIsAdmin} = require("../middleware/verifyAuthToken");
+const {getUserOrders,getOrder,createOrder,updateOrderToPaid,updateOrderToDelivered,getOrders} = require("../controllers/OrderController")
+
+// Route for users
+router.use(verifyIsLoggedIn);
+router.get("/", getUserOrders);
+router.get("/user/:id", getOrder);  // order details for perticular user
+router.post("/", createOrder);
+router.put("/paid/:id", updateOrderToPaid);
+
+// Route for Admin
+router.use(verifyIsAdmin);
+router.put("/delivered/:id", updateOrderToDelivered);
+router.get("/admin", getOrders);
+
+module.exports = router
