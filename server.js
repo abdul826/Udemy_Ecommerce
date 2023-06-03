b109d2380708bad084c9e2ababbaf2f3ecb4aaf0
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2023-06-03 10:33:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-const express = require('express')
-const app = express();
+const express = require("express");
 const fileUpload = require("express-fileupload");
-const port = 8000;
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = 5000;
 
-// Connect To Database
-const connectDb = require('./config/db');
-// connectDb();
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
 
-app.use(express.json());   // use to get the json data in postman
-app.use(fileUpload);        // use to upload file in postman
+const apiRoutes = require("./routes/apiRoutes");
 
-const apiRoutes = require("./routes/apiRoutes")
+app.get("/", async (req, res, next) => {
+  res.json({ message: "API running..." });
+});
 
-app.get('/', (req, res) => {
-    res.json({message: "API running..."})
-})
+// mongodb connection
+const connectDB = require("./config/db");
+connectDB();
 
-app.use('/api', apiRoutes)
+app.use("/api", apiRoutes);
 
+app.use((error, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    console.error(error);
+  }
+  next(error);
+});
+app.use((error, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    res.status(500).json({
+      message: error.message,
+      stack: error.stack,
+    });
+  } else {
+      res.status(500).json({
+         message: error.message, 
+      })
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
